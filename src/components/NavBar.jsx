d03c4d2c4a5be7db9fc297b53d2e1d84129c38f7
anyab <anyabelkina123@gmail.com>
2024-02-24 11:05:32 +0800

    login page started

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,7 @@ function NavBar() { return (
 <div> <nav>
         <Link to="/">Home</Link>
         <Link to="/project">Projects</Link>
+        <Link to="/login">Log In</Link>
       </nav>
       <Outlet />
     </div>
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,4 @@
+function LoginPage() {
+    return <h1>This is the login page</h1>;
+    }
+    export default LoginPage;
